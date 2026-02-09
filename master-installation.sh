a4590a44ed99966bf3bec44b4f03c5516274e77a
David Smithson <dwsmithson@gmail.com>
2026-02-08 23:44:36 -0700

    first commit

---
+++ master-installation.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+. ./install-stow.sh
+. ./install-tmux.sh
+. ./install-github-cli.sh
+. ./install-tailscale.sh
+. ./install-taskwarrior-tui.sh
+. ./install-timew.sh
+. ./install-vit.sh
+. ./install-openssh.sh
+. ./install-ssh.sh
+. ./install-mosh.sh
